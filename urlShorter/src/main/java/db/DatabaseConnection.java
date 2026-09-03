c02906a62267b541d64454110f0a72bff95f7ac2
nati439 <natichere77@gmail.com>
2026-09-02 21:39:10 -0500

    create a database connection

--- urlShorter/src/main/java/db/DatabaseConnection.java
+++ urlShorter/src/main/java/db/DatabaseConnection.java
@@ -1,3 +1,4 @@
+//This code creates a reusable method that connects your Java program to your MySQL nati database using the database URL, username, and password, then returns the connection.
 package db;
 
 import java.sql.Connection;
@@ -7,10 +8,12 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/nati";
+    //URL stores the JDBC connection URL, which tells Java where the MySQL database is located.
     private static final String USER = "nati";
     private static final String PASSWORD = "nati123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+    //DriverManager.getConnection(...) attempts to connect using the URL, username, and password, then returns the Connection object.
 }
